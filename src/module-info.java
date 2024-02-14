89106a0823159977224108a17554f90f633edfc0
Laura Silveira <laura.rra58@gmail.com>
2024-02-14 09:35:58 -0300

    About view

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module workshop.javafx.jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens gui to javafx.fxml;
 	opens application to javafx.graphics, javafx.fxml;
